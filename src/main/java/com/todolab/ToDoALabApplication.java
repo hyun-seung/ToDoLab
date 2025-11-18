e5fde83e0482dfab781c11750160f15d1a93b546
hyunseung <ardimento22@naver.com>
2025-11-18 16:36:13 +0900

    YML 환경 분리

---
+++ src/main/java/com/todolab/ToDoALabApplication.java
@@ -0,0 +1,12 @@
+package com.todolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoALabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoALabApplication.class, args);
+    }
+}
