5c822f504cb0c6d4de3e7d36d9e22d67eb7ed0ee
hyunseung <ardimento22@naver.com>
2026-02-04 00:16:27 +0900

    메일 발송(SMTP) 설정 및 로컬 발송 테스트 추가

---
+++ src/main/java/com/todolab/task/service/mail/MailSendTestRunner.java
@@ -0,0 +1,25 @@
+package com.todolab.task.service.mail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+//@Profile("local")
+//@Component
+@RequiredArgsConstructor
+public class MailSendTestRunner implements CommandLineRunner {
+
+    private final MailService mailService;
+
+    private static final String DEVELOPER_MAIL = "ardimento22@naver.com";
+
+    @Override
+    public void run(String... args) {
+        log.info("Mail send test start");
+        mailService.sendText(DEVELOPER_MAIL, "[ToDoLab] 메일 설정 테스트", "SMTP 테스트");
+        log.info("Mail send test fin");
+    }
+}
---
+++ src/main/java/com/todolab/task/service/mail/MailService.java
@@ -0,0 +1,33 @@
+package com.todolab.task.service.mail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    private static String FROM;
+
+    @Value("${spring.mail.username}")
+    private void setMailSender(String username) {
+        FROM = "ToDoLab <" + username + ">";
+    }
+
+    public void sendText(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(FROM);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+}
